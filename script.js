74390e338f724d3c335803fb4c93c8e92cfa428a
sahilsaifi310 <shahilsaifi750@gmail.com>
2025-01-08 17:48:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
- let inputBoxes = document.querySelectorAll('.code');
+document.addEventListener('DOMContentLoaded', (event) => {
+  var inputs = Array.from(document.querySelectorAll('.code'));
 
-        inputBoxes.forEach((input, index) => {
-            //
-            input.addEventListener('input', function() {
-                if (input.value.length == 1 && index < inputBoxes.length - 1) {
-                    inputBoxes[index + 1].focus();  
-                }
-            });
+  inputs.forEach((input, index) => {
+    input.addEventListener('input', () => {
+      if (input.value.length > 0) {
+        if (index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      }
+    });
 
-            
-            input.addEventListener('keydown', function(e) {
-                if (e.key === 'Backspace' && input.value.length === 0 && index > 0) {
-                    inputBoxes[index - 1].focus();  
-                }
-            });
-        });
+    input.addEventListener('keydown', (event) => {
+      if (event.key === 'Backspace' && input.value.length === 0) {
+        if (index > 0) {
+          inputs[index - 1].focus();
+        }
+      }
+    });
+  });
+
+  // Set the focus on the first input field when the page loads
+  if (inputs.length > 0) {
+    inputs[0].focus();
+  }
+});
