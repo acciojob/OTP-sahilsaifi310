13dad39170f5a15f17b10b0fe69c9cf84f4d5094
sahilsaifi310 <shahilsaifi750@gmail.com>
2025-01-08 17:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
-//your JS code here. If required.
-let inputElements = document.getElementsByClassName("code");
-console.log(inputElements.length)
-for(let i=0; i< inputElements.length; i++){
-	inputElements[i].addEventListener("keyup", (event)=>{
-		let currentElement = event.target ;
-		let code = event.key.charCodeAt(0);
-		console.log(event.key)
-		if(event.key === "Backspace"){
-			let prevElement = currentElement.previousElementSibling ;
-			if(prevElement){
-				prevElement.focus();
-			}
-			return ;
-		}
-		
-		
-		
-		if(code>=48 && code<=57){
-			let nextElement = currentElement.nextElementSibling ;
-			if(nextElement){
-				nextElement.focus() ;
-			}
-			
-		}else{
-			event.target.value="" ;
-		}
-	})
-	
-}
+ let inputBoxes = document.querySelectorAll('.code');
+
+        inputBoxes.forEach((input, index) => {
+            //
+            input.addEventListener('input', function() {
+                if (input.value.length == 1 && index < inputBoxes.length - 1) {
+                    inputBoxes[index + 1].focus();  
+                }
+            });
+
+            
+            input.addEventListener('keydown', function(e) {
+                if (e.key === 'Backspace' && input.value.length === 0 && index > 0) {
+                    inputBoxes[index - 1].focus();  
+                }
+            });
+        });
